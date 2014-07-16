c23ad5b02db3f2d2a2b3ac39e5d2cf3a7b5cf2aa
MALRMALR <michaelalross@gmail.com>
2014-07-16 14:28:21 -0400

    fixed reg exp in Post class for youtube and vimeo

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,11 +10,11 @@ class Post < ActiveRecord::Base
   def strip_link
     link = self.embed_link # sets variable link equal to embed link paramater of an instance of the class Post
     if link.include? 'youtube'
-      self.embed_link = link[-11..-1] # this sets embed_link paramater of instance of Post class to the last 10 digits of the link
+       self.embed_link = link.scan(/watch?v=\/\./).first # this sets embed_link paramater of instance of Post class to the last 10 digits of the link
     elsif link.include? 'vimeo'
-      self.embed_link = link[-9..-1]
+       self.embed_link = link.scan(/\/\d{1,}/).first
     elsif link.include? 'soundcloud'
-      self.embed_link = link.scan(/tracks\/\d{1,}/).first # Reg Exp scans embed link for 'tracks'/then all numbers', calls .first method on link variable and returns first colelction of characters (from a string) that meets this specification
+       self.embed_link = link.scan(/tracks\/\d{1,}/).first # Reg Exp scans embed link for 'tracks'/then all numbers', calls .first method on link variable and returns first colelction of characters (from a string) that meets this specification
     end
   end
 
